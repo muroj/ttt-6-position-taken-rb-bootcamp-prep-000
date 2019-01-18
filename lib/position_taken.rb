9278c05e2298e81167e352afd47af5610d892d3d
Joe Muro <jmuro0321@gmail.com>
2019-01-18 19:33:01 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   def position = board[index]
-  def empty = position.empty || position.eql?(" ") || position.nil?
+  def empty = position.empty? || position.eql?(" ") || position.nil?
   
   return !empty ? (position.eql?("X") || position.eql?("O")) : empty
   
