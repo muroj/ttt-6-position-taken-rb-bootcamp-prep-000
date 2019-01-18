2404de4897e9472756fa0f2b2f87e0442f7f8c36
Joe Muro <jmuro0321@gmail.com>
2019-01-18 19:27:35 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,4 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  board[index].empty || board[index]
+  board[index].empty || board[index].eql?(" ") || board[index]
 end
